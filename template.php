2fcb9b87afa7aca7bde0d2eb53c9001669fc3f91
antonioortegajr <antonioortegajr@gmail.com>
2017-04-29 09:03:55 -0700

    add image
    add main image

---
+++ template.php
@@ -0,0 +1,17 @@
+<?php
+/* Create your Template here using twig variables
+ * This template will be used by index.php
+ * index.php is an IDX Broker details page with default CSS
+*/
+$localTemplate = '
+
+<div>
+{{address}}
+</div>
+
+
+
+
+
+';
+ ?>
---
+++ variables/var.php
@@ -0,0 +1,15 @@
+<?php
+$address = '5353 SW 60 PL';
+$streetNumber = '5353';
+$streetDirection = 'N';
+$streetName = 'fake';
+$unitNumber = '1';
+$cityName = 'Miami Beach';
+$stateAbrv = 'FL';
+$state = 'Florida';
+$zipcode = '33140-4224';
+$propStatus = 'A';
+$priceLabel = '';
+$priceDisplay = '';
+$moreInfoLink = '';
+$scheduleShowingLink  = '';
